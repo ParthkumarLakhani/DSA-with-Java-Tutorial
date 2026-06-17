8482bba14782a6f82fd442e7b826b1737399568c
Parth Lakhani <parthlakhani003@gmail.com>
2026-06-18 00:13:19 +0530

    feat: remove white space

--- src/main/java/com/dsaWithJava/dsa/DsaApplication.java
+++ src/main/java/com/dsaWithJava/dsa/DsaApplication.java
@@ -1,6 +1,5 @@
 package com.dsaWithJava.dsa;
 
-import com.dsaWithJava.dsa.linear_binary_search_05.LinearSearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,8 +10,6 @@ public class DsaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DsaApplication.class, args);
 
-		LinearSearch l1 = new LinearSearch();
-		l1.linearSearch();
 	}
 
 }
