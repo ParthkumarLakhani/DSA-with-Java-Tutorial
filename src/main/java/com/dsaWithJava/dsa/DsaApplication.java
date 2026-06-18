f5b05245e6c1e13f4fca92fb6858415d017c92e0
Parth Lakhani <parthlakhani003@gmail.com>
2026-06-18 16:53:33 +0530

    feat: linear serach leet code

--- src/main/java/com/dsaWithJava/dsa/DsaApplication.java
+++ src/main/java/com/dsaWithJava/dsa/DsaApplication.java
@@ -1,5 +1,6 @@
 package com.dsaWithJava.dsa;
 
+import com.dsaWithJava.dsa.linear_binary_search_05.linearSearch.LinearSearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,8 @@ public class DsaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DsaApplication.class, args);
 
+		LinearSearch l1 = new LinearSearch();
+		l1.richestCustomer();
 	}
 
 }
--- src/main/java/com/dsaWithJava/dsa/linear_binary_search_05/linearSearch/LinearSearch.java
+++ src/main/java/com/dsaWithJava/dsa/linear_binary_search_05/linearSearch/LinearSearch.java
@@ -1,5 +1,14 @@
 package com.dsaWithJava.dsa.linear_binary_search_05.linearSearch;
 
+/* Time Complexity
+
+Basic Amount of time taken to perform operation.
+
+Time Complexity is a measure of how the execution time of an algorithm grows as the input size (n) increases.
+
+How The time grows as your input grows.
+*/
+
 public class LinearSearch {
 
     //search in int
@@ -88,13 +97,100 @@ public class LinearSearch {
         return false;
     }
 
-    /* Time Complexity
+    // leet code 1295
 
-    Basic Amount of time taken to perform operation.
+    public void findEvenNumberOfDigits(){
+        // ary of integers, return how many of them contain an even number of digits.
 
-    Time Complexity is a measure of how the execution time of an algorithm grows as the input size (n) increases.
+        int[] ary = { 12, 345, 2, 6, -7896, 89, 8789};
 
-    How The time grows as your input grows.
-    */
+        System.out.println(findEvenNumberOfDigitsNumber(ary));
+    }
+
+    // findEvenNumberOfDigits  ->  using string
+    public int findEvenNumberOfDigitsString(int[] ary ){
+        int count = 0;
+
+        for (int i = 0; i <= ary.length - 1; i++){
+
+            String str = String.valueOf(Math.abs(ary[i]));
+
+            if (str.length() % 2 == 0){
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+    // with no in built method
+    public int findEvenNumberOfDigitsNumber(int[] ary){
+        int count = 0;
+
+        for (int i = 0; i <= ary.length - 1; i++){
+            if(isEven(ary[i])){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public boolean isEven(int num){
+
+        if (num < 0){
+            num = num * -1;
+        }
+
+        return numDigit(num) % 2 == 0;
+    }
+
+    public int numDigit(int num){
+        int count = 0;
+
+        while ( num > 0){
+            count++;
+            num = num/10;
+        }
+
+        return count;
+    }
+
+    // with log method
+    public int withLog(int num){
+        return (int)(Math.log(num) / 10 ) + 1;
+    }
+
+
+    //leet code - 1672
+    public void richestCustomer(){
+        int[][] amount = {
+                {2,8,7},
+                {7,1,3},
+                {1,9,5}
+        };
+
+        System.out.println(maxWealth(amount));
+    }
+
+    public static int maxWealth(int[][] amt){
+
+        int max = 0;
+
+        for (int i = 0; i <= amt.length - 1; i++ ){
+            int subArraySum = 0;
+            for (int j = 0; j <= amt[i].length - 1; j++) {
+                subArraySum = subArraySum + amt[i][j];
+            }
+            if (subArraySum > max){
+                max = subArraySum;
+            }
+        }
+
+        return max;
+    }
+
+
+
 
 }
